e6722b2fca19f62eedef9dc37772727ad1103df5
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-06-03 15:48:35 +0300

    Generating correct BIP39 mnemonics :)

--- README.md
+++ README.md
@@ -1,33 +1,33 @@
 ```
 go get github.com/xaionaro/trezor-seed-generator/trezorSeedGenerator
-go run ~/go/src/github.com/xaionaro/trezor-seed-generator/trezorSeedGenerator/main.go -d /usr/lib/python3/dist-packages/mnemonic/wordlist/english.txt
+go run ~/go/src/github.com/xaionaro/trezor-seed-generator/trezorSeedGenerator/main.go
 ```
 
 result:
 ```
-$ go run ~/go/src/github.com/xaionaro/trezor-seed-generator/trezorSeedGenerator/main.go -d /tmp/american.mwl 
- 1: theory
- 2: hire
- 3: grunt
- 4: member
- 5: proof
- 6: wrong
- 7: gasp
- 8: glare
- 9: horse
-10: together
-11: stem
-12: retire
-13: solve
-14: useless
-15: army
-16: today
-17: pizza
-18: sense
-19: news
-20: episode
-21: hover
-22: recipe
-23: shoulder
-24: oak
+$ go run ~/go/src/github.com/xaionaro/trezor-seed-generator/trezorSeedGenerator/main.go
+ 1: helmet
+ 2: surround
+ 3: agree
+ 4: brand
+ 5: any
+ 6: enough
+ 7: supply
+ 8: choose
+ 9: bronze
+10: case
+11: rely
+12: crazy
+13: pigeon
+14: satoshi
+15: wrist
+16: abuse
+17: divide
+18: wall
+19: injury
+20: tourist
+21: example
+22: proud
+23: fashion
+24: castle
 ```
--- trezorSeedGenerator/main.go
+++ trezorSeedGenerator/main.go
@@ -3,52 +3,31 @@ package main
 import (
 	"crypto/rand"
 	"fmt"
-	"github.com/pborman/getopt/v2"
-	"math/big"
-	"strings"
-	"io/ioutil"
+	"github.com/tyler-smith/go-bip39"
 	"os"
-	"regexp"
+	"strings"
 )
 
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	//randomSourcePath := getopt.StringLong("random-source", 'r', "/dev/random", "the source of random number (secure) [default: /dev/random]")
-	dictionaryPath := getopt.StringLong("dictionary-path", 'd', "", "the source of random number (secure)")
+	var err error
 
-	getopt.Parse()
+	rand.Reader, err = os.Open("/dev/random")
+	checkError(err)
 
-	if *dictionaryPath == "" {
-		panic("dictionary path is not set")
-	}
+	entropy, err := bip39.NewEntropy(256)
+	checkError(err)
 
-	randomReader, err := os.Open("/dev/random")
-	if err != nil {
-		panic(err)
-	}
+	mnemonic, err := bip39.NewMnemonic(entropy)
+	checkError(err)
 
-	dictionary, err := ioutil.ReadFile(*dictionaryPath)
-	if err != nil {
-		panic(err)
-	}
-	dictionaryLines := strings.Split(string(dictionary), "\n")
-	dictionaryLinesCountBigInt := big.NewInt(int64(len(dictionaryLines)))
-
-	re := regexp.MustCompile("[^a-z].*")
-	wordsCount := 0
-	for {
-		randValue, err := rand.Int(randomReader, dictionaryLinesCountBigInt)
-		if err != nil {
-			panic(err)
-		}
-		word := strings.ToLower(dictionaryLines[randValue.Int64()])
-		word = re.ReplaceAllString(word, "")
-		if len(word) < 3 {
-			continue
-		}
-		wordsCount++
-		fmt.Printf("%2d: %v\n", wordsCount, word)
-		if wordsCount >= 24 {
-			break
-		}
+	words := strings.Split(mnemonic, " ")
+	for idx, word := range words {
+		fmt.Printf("%2d: %s\n", idx+1, word)
 	}
 }
